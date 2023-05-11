1b2ad2d178caa4a83b143ac2cff7ab1fe8bedaf8
beatsbydan <danielonyeachonam2020@gmail.com>
2023-05-11 17:26:30 +0100

    Done

--- src/AddOnContext/AddOnContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-const AddOnContext = React.createContext({
-    plan: '',
-    addOn: []
-})
-export default AddOnContext;
--- src/AddOnContext/AddOnContextProvider/AddOnContextProvider.js
+++
@@ -1,8 +0,0 @@
-import AddOnContext from '../AddOnContext'
-const  AddOnContextProvider = (props) => {
-        <AddOnContext.Provider>
-            {props.children}
-        </AddOnContext.Provider>
-}
- 
-export default  AddOnContextProvider;
